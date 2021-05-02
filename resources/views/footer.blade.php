061a3e44684a6f2bd7217cf621a9c41de7c64de1
farhan0 <farhanthegamer76@gmail.com>
2021-05-02 06:37:36 +0500

    header, footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer mt-auto py-3 bg-light">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  <div class="container">
+    <span class="text-muted">Place sticky footer content here.</span>
+  </div>
+</footer>
